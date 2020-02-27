02a0ee5afe8eed0d5aafc2e3c74e19b0878c3b1a
sen <1528094223@qq.com>
2020-02-27 18:40:27 +0800

    Initial commit

---
+++ src/com/atguigu/idea/Test.java
@@ -0,0 +1,18 @@
+package com.atguigu.idea;
+
+/**
+ * @author Administrator
+ * @create 2020-02-27 13:42
+ */
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            for (String arg : args) {
+                while (true) {
+
+                }
+            }
+
+        }
+    }
+}
